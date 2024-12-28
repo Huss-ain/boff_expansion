c8891834fa7c5193b9cbf6b86ed8aaf9abeb2cc8
Huss-ain <151734377+Huss-ain@users.noreply.github.com>
2024-12-28 15:36:02 +0000

    deployment issue

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -10,103 +10,104 @@ import avatar4 from "@/assets/arabwoman 2.jpg";
 import middleEast from "@/assets/middle-east map.jpg";
 import leads from "@/assets/leads image.png";
 
+// Define a type for features
+type Feature = {
+  title: string;
+  description: string;
+  image: JSX.Element; // Store the JSX structure for the image
+};
 
-const features = [
-    
+const features: Feature[] = [
+  {
+    title: "Instant leads & discovery",
+    description: "We'll start the discovery process to find you potential customers.",
+    image: (
+      <Image
+        src={leads}
+        alt="sales leads image"
+        height={260}
+        width={260}
+        className="rounded"
+      />
+    ),
+  },
+  {
+    title: "Local SDR professionals",
+    description: "Culture and language-aware professionals that can take care of pre- and post-sales support.",
+    image: (
+      <div className="aspect-video flex items-center justify-center">
+        <Avatar className="border-yellow-100 z-40">
+          <Image 
+            src={avatar1} 
+            alt="picture of arab man"
+            className="w-16 h-16 object-cover"
+          />
+        </Avatar>
+        <Avatar className="-ml-6 border-yellow-500 z-30">
+          <Image 
+            src={avatar3} 
+            alt="picture of arab woman"
+            className="w-16 h-16 object-cover"
+          />
+        </Avatar>
+        <Avatar className="-ml-6 z-20">
+          <Image 
+            src={avatar2} 
+            alt="picture of arab man 2"
+            className="w-16 h-16 object-cover"
+          />
+        </Avatar>
+        <Avatar className="-ml-6 border-yellow-700">
+          <Image 
+            src={avatar4} 
+            alt="picture of arab woman 2"
+            className="w-16 h-16 object-cover"
+          />
+        </Avatar>
+      </div>
+    ),
+  },
+  {
+    title: "Fully compliant across the region",
+    description: "In a matter of days, you'll be ready to sell wherever the opportunities exist.",
+    image: (
+      <div className="relative">
+        <div className="absolute inset-0 bg-yellow-100 opacity-20"></div>
+        <Image
+          src={middleEast}
+          alt="map of middle east"
+          className="rounded aspect-square border border-yellow-950 h-56"
+        />
+      </div>
+    ),
+  },
 ];
 
 export function Features() {
-    return (
-        <section id="features" className="py-24">
-            <div className="container mx-auto px-4">
-                <div className="flex justify-center">
-                    <div  className="tag "> Features</div>
-                </div>
-                <div className="max-w-[740px] mx-auto">
-                <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#D8AE7E] text-transparent bg-clip-text  mt-7 ml-6">A new method for <span className="">market entry</span></h2>
-                <p className="section-description"> 
-                    Don&apos;t wait months for feasibility plans and assessments, 
-                    start testing the market immediately with Neogulf. From initial leads to
-                    professionals on the ground, we ensure your expansion is merely
-                    a process and not a gamble.
-                </p>
-                </div>
-                <div className="mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 md:col-start-2 lg:grid-cols-3 items-stretch items-center justify-items-center">
-                    <FeatureCard
-                        title = "Instant leads & discovery"
-                        description="We'll start the discovery process to find you potential customers"
-                    >
-                        <div>
-                            <Image
-                            src= {leads}
-                            alt="sales leads image"
-                            height={260}
-                            width={260}
-                            className="rounded"
-                            />
-                        </div>
-                    </FeatureCard>
-                    <FeatureCard
-                        title = "Local SDR professionals"
-                        description="Culture and language aware professionals that can take care of pre and post sales support"
-                    >
-                        <div className="aspect-video flex items-center justify-center">
-                            <Avatar className="border-yellow-100 z-40">
-                                <Image 
-                                src={avatar1} 
-                                alt="picture of arab man"
-                                className="w-16 h-16 object-cover"
-                                />
-                            </Avatar>
-                            <Avatar className="-ml-6 border-yellow-500 z-30">
-                                <Image 
-                                src={avatar3} 
-                                alt="picture of arab man2"
-                                className="w-16 h-16 object-cover"
-                                />
-                            </Avatar>
-                            <Avatar className="-ml-6 z-20 ">
-                                <Image 
-                                src={avatar2} 
-                                alt="picture of arab woman"
-                                className="w-16 h-16 object-cover"
-                                />
-                            </Avatar>
-                            <Avatar className="-ml-6 border-yellow-700">
-                                <Image 
-                                src={avatar4} 
-                                alt="picture of arab woman"
-                                className="w-16 h-16 object-cover"
-                                />
-                            </Avatar>
-                        </div>
-                    </FeatureCard>
-                    <FeatureCard
-                        title = "Fully compliant across the region"
-                        description="In a matter of days, you'll be ready to sell wherever the opportunities exist"
-                    >
-                        <div className="relative">
-                            <div className="absolute inset-0 bg-yellow-100 opacity-20"></div>
-                            <Image
-                            src={middleEast}
-                            alt="map of middle east"
-                            className="rounded aspect-square border border-yellow-950 h-56"
-                            
-                            />
-                            
-                        </div>
-                        </FeatureCard>      
-                </div>
-                <div>
-                    {features.map((feature) => (
-                        <div key={feature}>
-                            <span></span>
-                            <span>{feature}</span>
-                         </div>
-                    ))}
-                </div>
-            </div>
-
-        </section>
-    );
+  return (
+    <section id="features" className="py-24">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-center">
+          <div className="tag">Features</div>
+        </div>
+        <div className="max-w-[740px] mx-auto">
+          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#D8AE7E] text-transparent bg-clip-text mt-7 ml-6">
+            A new method for <span>market entry</span>
+          </h2>
+          <p className="section-description">
+            Don&apos;t wait months for feasibility plans and assessments, start testing the market immediately
+            with Neogulf. From initial leads to professionals on the ground, we ensure your expansion is merely
+            a process and not a gamble.
+          </p>
+        </div>
+        <div className="mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 items-stretch items-center justify-items-center">
+          {features.map((feature, index) => (
+            <FeatureCard key={index} title={feature.title} description={feature.description}>
+              {feature.image}
+            </FeatureCard>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
