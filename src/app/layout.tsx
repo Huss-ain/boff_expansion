cf4c3a9a3f06d98cd176d452e017628121bdd4c7
Huss-ain <151734377+Huss-ain@users.noreply.github.com>
2024-12-29 00:23:18 +0000

    namechange

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import clsx from "clsx";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Neogulf - Tech for the Middle East",
+  description: "Expand your tech to the world's richest market",
 };
 
 export default function RootLayout({
